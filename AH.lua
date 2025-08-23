0d7624f0815b07dc3d04b79efc457dc311389104
Bant3r241 <bobby36ko@outlook.com>
2025-08-22 23:53:25 -0400

    Update AH.lua

--- AH.lua
+++ AH.lua
@@ -2,7 +2,28 @@ if game.PlaceId == 118554563202898 then
     local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
     local Window = OrionLib:MakeWindow({Name="ABI │ AnimeHunters", HidePremium=false, IntroEnabled=false, IntroText="ABI", SaveConfig=true, ConfigFolder="XlurConfig"})
 
- local MainTab = Window:MakeTab({Name = "Main"})
+local Tab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
+
+
+    OrionLib:MakeNotification({
+	Name = "ABI",
+	Content = "Loading Script!",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+Tab:AddBind({
+	Name = "Bind",
+	Default = Enum.KeyCode.f,
+	Hold = false,
+	Callback = function()
+		print("press")
+	end    
+})
 
 OrionLib:Init()
