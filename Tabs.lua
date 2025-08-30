23c9c70e5dace0f701b67869303a78fc0cfa750f
Bant3r <bobby36ko@outlook.com>
2025-08-30 05:27:43 -0400

    Update Tabs.lua

--- Tabs.lua
+++ Tabs.lua
@@ -1,10 +1,30 @@
 return function(Window, OrionLib)
-    
-
     local MainTab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998"})
     
+    MainTab:AddButton({
+        Name = "Example Button",
+        Callback = function()
+            print("Button clicked!")
+        end
+    })
+    
     local ESPTab = Window:MakeTab({Name = "ESP", Icon = "rbxassetid://4483345998"})
-  
+    
+    ESPTab:AddToggle({
+        Name = "Enable ESP",
+        Default = false,
+        Callback = function(Value)
+            print("ESP:", Value)
+        end
+    })
+    
     local SettingsTab = Window:MakeTab({Name = "Settings", Icon = "rbxassetid://4483345998"})
     
- 
+    SettingsTab:AddKeybind({
+        Name = "UI Toggle Key",
+        Default = Enum.KeyCode.RightShift,
+        Callback = function()
+            Window:Toggle()
+        end
+    })
+end
