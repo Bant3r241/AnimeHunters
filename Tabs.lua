9ca12fe3951ec90a1e26af2ea35035441c2af618
Bant3r <bobby36ko@outlook.com>
2025-08-30 05:24:31 -0400

    Create Tabs.lua

---
+++ Tabs.lua
@@ -0,0 +1,10 @@
+return function(Window, OrionLib)
+    
+
+    local MainTab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998"})
+    
+    local ESPTab = Window:MakeTab({Name = "ESP", Icon = "rbxassetid://4483345998"})
+  
+    local SettingsTab = Window:MakeTab({Name = "Settings", Icon = "rbxassetid://4483345998"})
+    
+ 
